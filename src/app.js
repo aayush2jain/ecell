3fb9408137536167cdf14941cd62204c4e88d519
aayush jain <aayushjain1290@gmail.com>
2024-10-28 15:17:41 +0530

    kop

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 const app=express();
 
 app.use(cors({
-  origin: ['https://ecellfrontend-5q7u.vercel.app', 'http://localhost:3000'],
+  origin: ['https://ecellfrontend-5q7u.vercel.app', 'http://localhost:3000','https://www.ecelldtu.in/'],
   credentials: true
 }));
 
