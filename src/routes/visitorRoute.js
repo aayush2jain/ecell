f274cd00e64a066bcae31c03e2a046e213a79a4c
aayush jain <aayushjain1290@gmail.com>
2025-01-15 08:46:26 +0530

    chalja

--- src/routes/visitorRoute.js
+++ src/routes/visitorRoute.js
@@ -1,34 +1,37 @@
 import { Router } from "express";
 import Visitor from "../models/visitor.model.js";
 const router = Router();
-router.post('/',async (req,res)=>{
-  const { name,college,city,email,contact,gender } = req.body;
-  try{
-    //  const registeredUser = await Visitor.findOne({email:email})
-    //  if(registeredUser){
-    //     return res.send("You have already applied")
-    //  }
+router.post('/', async (req, res) => {
+  const { name, college, city, email, contact, gender } = req.body;
 
-    const visitor = new Visitor({
-    name,
-    college,
-    city,
-    email,
-    contact,
-    gender,
-     }
-     )
-     const newvisitor  = await visitor.save();
-     if(newvisitor){
-        return res.status(200).json({ mesaage:"You have sucessessfully register"})
-     }
-     else{
-        return res.status(500).json({message:"something went wrong"})
-     }
+  // Validate input fields
+  if (!name || !college || !city || !email || !contact || !gender) {
+    return res.status(400).json({ message: "All fields are required" });
   }
-catch(error){
-    return res.status(500).json({message:"not able to register"})
-}
-})
+
+  try {
+    // Create a new visitor
+    const visitor = new Visitor({
+      name,
+      college,
+      city,
+      email,
+      contact,
+      gender,
+    });
+
+    // Save the visitor to the database
+    const newVisitor = await visitor.save();
+
+    if (newVisitor) {
+      return res.status(200).json({ message: "You have successfully registered" });
+    } else {
+      return res.status(500).json({ message: "Something went wrong" });
+    }
+  } catch (error) {
+    console.error("Error saving visitor:", error);
+    return res.status(500).json({ message: "Unable to register, please try again later" });
+  }
+});
 
 export default router;
